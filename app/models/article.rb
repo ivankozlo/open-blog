aab7153a573aca76d31b761016b50d41e17cd260
ivankozlo <ivan.olegovich.kozlov@gmail.com>
2021-03-06 15:52:53 +1000

    Fixed article display style

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
   has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 3000 }
   validates :user_id, presence: true
 end
